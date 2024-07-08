d939f3703d46afba9e960c964f90f9ed1ccada3d
Srimanta Dey <srimantade35@gmail.com>
2024-07-08 18:34:11 +0530

    button change

--- frontend/src/components/CardProduct.jsx
+++ frontend/src/components/CardProduct.jsx
@@ -34,7 +34,7 @@ function CardProduct({items}) {
       <p className='line-clamp-2 text-ellipsis font-semibold'>{items?.name}</p>
         <div className='flex items-center gap-3 justify-between'>
             <p className='text-black font-bold '>₹{items?.price}</p>
-            <button className='bg-orange-600 text-white px-2 py-1 rounded hover:bg-orange-700 mr-1 min-w-[100px] ' onClick={handleAddToCart}>Add To Cart</button>
+            <button className='bg-orange-600 text-white px-2 py-1 rounded hover:bg-orange-700 mr-1' onClick={handleAddToCart}>Add To Cart</button>
         </div>
    </Link>
   )

8b5f3374532d42bb568d73af2cbeaafa785ffe5f
Srimanta Dey <srimantade35@gmail.com>
2024-07-08 18:39:57 +0530

    again button change

--- frontend/src/components/CardProduct.jsx
+++ frontend/src/components/CardProduct.jsx
@@ -22,7 +22,7 @@ function CardProduct({items}) {
 
   return (
    
-   <Link to={"/menu/"+items._id} onClick={()=>window.scrollTo({top:"0",behavior:'smooth'})} className='md:w-44 w-25 h-25 md:min-w-52 shadow-md p-2 grid gap-1  md:max-w-52 mr-2 ml-2 text-center '>
+   <Link to={"/menu/"+items._id} onClick={()=>window.scrollTo({top:"0",behavior:'smooth'})} className='md:w-44 w-25 h-25 md:min-w-52 shadow-md p-2 grid gap-1  md:max-w-52 mr-1 ml-1 text-center '>
       <div className='w-36 mx-auto '>
          <img 
          src={items?.image}
@@ -32,9 +32,9 @@ function CardProduct({items}) {
       </div>
       <p className='text-black font-medium text-sm'>{items?.category}</p>
       <p className='line-clamp-2 text-ellipsis font-semibold'>{items?.name}</p>
-        <div className='flex items-center gap-3 justify-between'>
+        <div className='flex items-center gap-2 md:gap-3 justify-between'>
             <p className='text-black font-bold '>₹{items?.price}</p>
-            <button className='bg-orange-600 text-white px-2 py-1 rounded hover:bg-orange-700 mr-0.5' onClick={handleAddToCart}>Add To Cart</button>
+            <button className='bg-orange-600 text-white px-2 py-1 rounded hover:bg-orange-700' onClick={handleAddToCart}>Add To Cart</button>
         </div>
    </Link>
   )

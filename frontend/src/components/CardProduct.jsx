caa2e5bbe216c0163f7559e02ec53d23ed7ef267
Srimanta Dey <srimantade35@gmail.com>
2024-07-08 18:53:53 +0530

    Update CardProduct.jsx

--- frontend/src/components/CardProduct.jsx
+++ frontend/src/components/CardProduct.jsx
@@ -32,7 +32,7 @@ function CardProduct({items}) {
       </div>
       <p className='text-black font-medium text-sm'>{items?.category}</p>
       <p className='line-clamp-2 text-ellipsis font-semibold'>{items?.name}</p>
-        <div className='flex items-center gap-1 md:gap-3 justify-between'>
+        <div className='flex items-center gap-3 justify-between'>
             <p className='text-black font-bold '>₹{items?.price}</p>
             <button className='bg-orange-600 text-white px-2 py-1 rounded hover:bg-orange-700 mr-0.5' onClick={handleAddToCart}>Add To Cart</button>
         </div>

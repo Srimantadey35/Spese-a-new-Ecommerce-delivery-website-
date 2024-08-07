b125f6340dedbaea3f17481378f8c8fe5e1c30ad
Srimanta Dey <srimantade35@gmail.com>
2024-08-07 15:31:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Outlet } from 'react-router-dom'
+import { Outlet,useNavigate } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import toast, {Toaster} from "react-hot-toast"
 import { useSelector,useDispatch } from 'react-redux'
 import axios from 'axios'
-import { setProducts, setUser } from '../store/userSlice.js'
+import { setProducts, setUser,setToken } from '../store/userSlice.js'
 import Footer from "./pages/Footer.jsx";
 
 function App() {
+  const navigate = useNavigate()
   const accessToken = useSelector((state)=>state.user.accessToken)
   const dispatch = useDispatch()
   const fetchUserDetails = async ()=>{
@@ -28,12 +29,27 @@ const fetchProductsDetails = async()=>{
     }
 }
 
+const refreshToken = async()=>{
+  const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/user/refreshToken`)
+  console.log("response",response)
+  if(response.data.success){
+    dispatch(setToken({accessToken : response.data.data.accessToken,refreshToken: response.data.data.refreshToken}))
+    localStorage.setItem("accessToken",response.data.data.accessToken)
+    localStorage.setItem("refreshToken",response.data.data.refreshToken)
+    navigate("/")
+  }
+}
+
 useEffect(()=>{
   fetchUserDetails()
 },[accessToken])
 
 useEffect(()=>{
   fetchProductsDetails()
+},[])
+
+useEffect(()=>{
+  refreshToken()
 },[])
 
 
